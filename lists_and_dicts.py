279c344ad6dcd139cc3a4fe7baa9bc1a1a2b7b5e
jonathan11ch <jonathan11ch@hotmail.com>
2022-03-30 23:41:42 -0400

    first line of code

---
+++ lists_and_dicts.py
@@ -0,0 +1,23 @@
+def run():
+	my_list = [1, "hello", 4.5]
+	my_dict = {"first_name": "jonathan", "last_name": "casas"}
+
+	super_list = [
+		{"first_name": "jonathan", "last_name": "casas"},
+		{"first_name": "diego", "last_name": "casas"},
+		{"first_name": "lina", "last_name": "bocanegra"}
+	]
+
+	super_dict = {
+		"natural_nums" : [1,2,3,4,5],
+		"integers" : [-1,-2,0,-1,-2],
+		"floating" : [1.2, 4.5, 6.43]
+	}
+
+
+	for key, value in super_dict.items():
+		print(key, "-", value)
+
+
+if __name__=='__main__':
+	run()
